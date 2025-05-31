c3759504b1ece734b1db5ec03f566376922d79ad
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-05-31 00:31:15 -0300

    correções de bugs reportados nos 47 cenarios de teste de login

--- src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
+++ src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
@@ -10,7 +10,7 @@ public class CcpCronTasksController {
 
 	public static void main(Function<CcpJsonRepresentation, CcpJsonRepresentation> jnAsyncBusinessNotifyError, String topic, String parameters) throws Exception {
 		CcpJsonRepresentation json = new CcpJsonRepresentation(parameters);
-		CcpMensageriaReceiver receiver = new CcpMensageriaReceiver(topic, "operation");
+		CcpMensageriaReceiver receiver = CcpMensageriaReceiver.getInstance(json);
 		CcpTopic process = receiver.getProcess(topic, json);
 		process.apply(json);
 
