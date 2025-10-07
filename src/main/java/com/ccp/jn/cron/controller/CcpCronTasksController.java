633ac38af2d95b376da754eeefca7f3c40f13e3d
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-10-07 18:03:31 -0300

    validação de fields do json, a saga continua...

--- src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
+++ src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
@@ -4,14 +4,13 @@ import java.util.function.Function;
 
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.especifications.mensageria.receiver.CcpMensageriaReceiver;
-import com.ccp.especifications.mensageria.receiver.CcpTopic;
 
 public class CcpCronTasksController {
 
 	public static void main(Function<CcpJsonRepresentation, CcpJsonRepresentation> jnAsyncBusinessNotifyError, String topic, String parameters) throws Exception {
 		CcpJsonRepresentation json = new CcpJsonRepresentation(parameters);
 		CcpMensageriaReceiver receiver = CcpMensageriaReceiver.getInstance(json);
-		CcpTopic process = receiver.getProcess(topic, json);
+		Function<CcpJsonRepresentation, CcpJsonRepresentation> process = receiver.getProcess(topic, json);
 		process.apply(json);
 
 	}
